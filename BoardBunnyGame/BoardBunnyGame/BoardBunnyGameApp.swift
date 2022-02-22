3f128e0ade58c0287c8ff3e05a9ca100e9dd32cf
Ilya Maslau <imaslov@gmail.com>
2022-02-23 00:05:32 +0300

    START new project by hello world

---
+++ BoardBunnyGame/BoardBunnyGame/BoardBunnyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardBunnyGameApp.swift
+//  BoardBunnyGame
+//
+//  Created by Ilya Maslau on 23.02.22.
+//
+
+import SwiftUI
+
+@main
+struct BoardBunnyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
